98408086a47623034cd216b1bb8369ce20daecbf
radith123 <rfadillah1641@gmail.com>
2024-04-19 15:52:28 +0700

    Reapply "Alamat Service"
    This reverts commit 620f2f9c511500f8c767e95758ecea0200678f05.

---
+++ src/test/java/com/webclient/alamat/AlamatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webclient.alamat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlamatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
